0fd4553d2ae551dbbb7406cf5511943b50dc6603
cesar-win10-pc <ces1406@gmail.com>
2023-10-02 14:43:44 -0300

    presentando resultados del pedido api en 1 lista

--- app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/OrdersState.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/OrdersState.kt
@@ -1,9 +1,13 @@
 package ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,6 +18,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.navigation.NavController
 import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.apiReqs.RetrofitHelper
 import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.stateData.EstadoPedidos
@@ -28,12 +34,27 @@ fun OrdersState(navCont: NavController) {
         content = { innerPadding ->
             Column (modifier = Modifier.fillMaxSize()){
                 Text(
-                    modifier = Modifier.fillMaxWidth().padding(innerPadding).wrapContentSize(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(innerPadding)
+                        .wrapContentSize(),
                     text = "/* SE CARGA UNA LISTA CON LOS ESTADOS DE LOS PEDIDOS DE LOS COMENSALES */"
                 )
                 LazyColumn(modifier = Modifier.fillMaxWidth()){
+                    item {
+                        Row (horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ){
+                            Text(fontWeight = FontWeight.Bold, text ="Nombre")
+                            Text(fontWeight = FontWeight.Bold,text = "Cantidad")
+                            Text(fontWeight = FontWeight.Bold,text = "Estado")
+                        }
+                    }
                     items(viewmodelo.state.pedidosMesa){e ->
-                        Row {
+                        Row (horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ){
+                            Spacer(modifier = Modifier.height(Dp(18F)))
                             Text(text = e.Plato.nombre)
                             Text(text = e.cantidad.toString())
                             Text(text = e.estado)
@@ -42,7 +63,10 @@ fun OrdersState(navCont: NavController) {
                 }
                 Button(
                     onClick = {navCont.navigate(route="mainmenu")},
-                    modifier = Modifier.fillMaxWidth().padding(innerPadding).wrapContentSize()
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(innerPadding)
+                        .wrapContentSize()
                 ) {
                     Text(text = "volver al menu")
                 }
