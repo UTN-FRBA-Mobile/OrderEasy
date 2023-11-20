4d1981bff65085a4ecc7714b682f61d82ecf2ffc
Federico Palumbo <fedepalu87@gmail.com>
2023-11-20 06:02:56 -0300

    screen main menu

--- app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/MainMenu.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/MainMenu.kt
@@ -1,75 +1,106 @@
 package ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainMenu(navCont: NavController) {
-    Scaffold (
+    Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "BARRA SUPERIOR DE LA APP") })
+            OderEasyTopAppBar()
         },
         content = { innerPadding ->
-            Column (modifier = Modifier.fillMaxSize()){
-                Button(
-                    onClick = {navCont.navigate(route="scanqr")},
-                    modifier = Modifier.fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Escanear QR")
+            LazyColumn(
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    top = 16.dp + innerPadding.calculateTopPadding(),
+                    end = 16.dp,
+                    bottom = 16.dp + innerPadding.calculateBottomPadding()
+                ),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                item {
+                    MenuButton("Escanear QR", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "scanqr")
+                    }
                 }
-                Button(
-                    onClick = {navCont.navigate(route="readmenu")},
-                    modifier = Modifier
-                        .fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Ver carta/menu")
+                item {
+                    MenuButton("Ver carta/menu", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "readmenu")
+                    }
                 }
-                Button(
-                    onClick = {navCont.navigate(route="makeorder")},
-                    modifier = Modifier
-                        .fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Hacer el pedido")
+                item {
+                    MenuButton("Hacer el pedido", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "makeorder")
+                    }
                 }
-                Button(
-                    onClick = {navCont.navigate(route="requestticket")},
-                    modifier = Modifier
-                        .fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Pedir la cuenta")
+                item {
+                    MenuButton("Pedir la cuenta", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "requestticket")
+                    }
                 }
-                Button(
-                    onClick = {navCont.navigate(route="callmozo")},
-                    modifier = Modifier
-                        .fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Llamar mozo")
+                item {
+                    MenuButton("Llamar mozo", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "callmozo")
+                    }
                 }
-                Button(
-                    onClick = {navCont.navigate(route="ordersstate")},
-                    modifier = Modifier
-                        .fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Ver pedidos de la mesa")
+                item {
+                    MenuButton("Ver pedidos de la mesa", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "ordersstate")
+                    }
                 }
-                Button(
-                    onClick = {navCont.navigate(route="closetable")},
-                    modifier = Modifier
-                        .fillMaxWidth().padding(innerPadding).wrapContentSize()
-                ) {
-                    Text(text = "Retirarse de la mesa")
+                item {
+                    MenuButton("Retirarse de la mesa", R.drawable.icon_notification_pay) { //TODO: buscar icono para el boton
+                        navCont.navigate(route = "closetable")
+                    }
                 }
             }
         }
     )
+}
+
+@Composable
+fun MenuButton(text: String, @DrawableRes iconId: Int, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+            .wrapContentSize(),
+        colors = ButtonDefaults.buttonColors(containerColor = Color.Gray),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = iconId),
+                contentDescription = null,
+                tint = Color.White
+            )
+            Text(text = text)
+        }
+    }
 }
