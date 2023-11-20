09f891326b080ea1a1ddabc1b77bc792c9b644ed
Federico Palumbo <fedepalu87@gmail.com>
2023-11-20 06:07:11 -0300

    screen call mozo

--- app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/CallMozo.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/CallMozo.kt
@@ -1,35 +1,56 @@
 package ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 @Composable
 fun CallMozo(navCont: NavController) {
     Scaffold (
         topBar = {
-            TopAppBar(title = { Text(text = "BARRA SUPERIOR DE LA APP")})
+            OderEasyTopAppBar()
         },
         content = { innerPadding ->
-            Column (modifier = Modifier.fillMaxSize()){
-                Text(
-                    modifier = Modifier.fillMaxWidth().padding(innerPadding).wrapContentSize(),
-                    text = "/* SE CARGA ALGUN BOTON/SIMIL PARA PEDIR LA ATENCIÓN DEL MOZO */"
-                )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
                 Button(
-                    onClick = {navCont.navigate(route="mainmenu")},
-                    modifier = Modifier.fillMaxWidth().padding(innerPadding).wrapContentSize()
+                    onClick = { /* TODO: Lógica para llamar al mozo */ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
                 ) {
-                    Text(text = "volver al menu")
+                    Text(text = "Llamar al Mozo")
                 }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(
+                    onClick = { navCont.navigate(route = "mainmenu") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                ) {
+                    Text(text = "Volver al Menú")
+                }
+
             }
         }
     )
