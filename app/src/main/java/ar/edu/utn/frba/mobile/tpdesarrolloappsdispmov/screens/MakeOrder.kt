c37d82b3b8c11b858e61a0ceefeb2ad52fee87ad
tomas.martinez <56210006+tmpic@users.noreply.github.com>
2023-11-26 18:57:00 -0300

    Pongo mensaje de alerta cuando el carrito esta vacio.

--- app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/MakeOrder.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/tpdesarrolloappsdispmov/screens/MakeOrder.kt
@@ -1,37 +1,33 @@
 package ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.screens
 
-import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -39,25 +35,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.R
 import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.components.TopBar
 import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.stateData.MenuViewModel
 import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.stateData.Plato
 import ar.edu.utn.frba.mobile.tpdesarrolloappsdispmov.stateData.UserViewModel
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 
 @Composable
 fun MakeOrder (navCont: NavController, menu: MenuViewModel, userViewModel: UserViewModel) {
@@ -71,18 +63,25 @@ fun MakeOrder (navCont: NavController, menu: MenuViewModel, userViewModel: UserV
             Column (modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)){
-                LazyColumn(modifier = Modifier.fillMaxWidth()){
-                    items(menu.estadoMenu.pedidos){p ->
-                        if(p.estado == "selected"){
-                            var e = menu.estadoMenu.platos.find { m -> m.idPlato == p.idPlato }
-                            if (e != null) {
-                                var quantity = menu.estadoMenu.pedidos.find{p -> (p.idPlato == e.idPlato && p.estado == "selected")}?.cantidad?:0
-                                FoodCardMakeOrder(food = e, quantity = quantity, modifier = Modifier.padding(16.dp), onAddToCart =  {
-                                    menu.addItem(e.idPlato)
-                                },
-                                onRemoveToCart =  {
-                                    menu.delItem(e.idPlato)
-                                })
+                if (menu.estadoMenu.pedidos.isEmpty()) {
+                    EmptyCartMessage(navCont)
+                } else {
+                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                        items(menu.estadoMenu.pedidos) { p ->
+                            if (p.estado == "selected") {
+                                var e = menu.estadoMenu.platos.find { m -> m.idPlato == p.idPlato }
+                                if (e != null) {
+                                    var quantity = menu.estadoMenu.pedidos.find { p -> (p.idPlato == e.idPlato && p.estado == "selected") }?.cantidad ?: 0
+                                    FoodCardMakeOrder(food = e,
+                                        quantity = quantity,
+                                        modifier = Modifier.padding(16.dp),
+                                        onAddToCart = {
+                                            menu.addItem(e.idPlato)
+                                        },
+                                        onRemoveToCart = {
+                                            menu.delItem(e.idPlato)
+                                        })
+                                }
                             }
                         }
                     }
@@ -124,6 +123,7 @@ fun FoodCardMakeOrder(food: Plato, quantity: Int, modifier: Modifier, onAddToCar
 
 @Composable
 fun BottomAppBarMakeOrder(navCont: NavController, menu: MenuViewModel, userViewModel: UserViewModel) {
+    var showDialog by remember { mutableStateOf(false) }
 
     val gradientBrush = Brush.verticalGradient(
         colors = listOf(Color.DarkGray, Color.White),
@@ -160,8 +160,12 @@ fun BottomAppBarMakeOrder(navCont: NavController, menu: MenuViewModel, userViewM
 
             Button(
                 onClick = {
-                    menu.orderItem(userViewModel.estadoUser.idMesa,userViewModel.estadoUser.idCliente)
-                    navCont.navigate(route="mainmenu")
+                    if(menu.estadoMenu.pedidos.size > 0){
+                        menu.orderItem(userViewModel.estadoUser.idMesa,userViewModel.estadoUser.idCliente)
+                        navCont.navigate(route="mainmenu")
+                    } else {
+                        showDialog = true
+                    }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -175,5 +179,53 @@ fun BottomAppBarMakeOrder(navCont: NavController, menu: MenuViewModel, userViewM
                 Text(text = "Confirmar pedido", color= Color.Black)
             }
         }
+
+        if (showDialog) {
+            AlertDialog(
+                containerColor = Color(251, 201, 143, 255),
+                icon = { Icon(Icons.Default.Info, "make-order") },
+                title = { Text(text = "Ups") },
+                text = { Text(text = "Necesitas tener platos en tu carrito para confirmar el pedido") },
+                onDismissRequest = { showDialog = false },
+                confirmButton = {
+                    TextButton(
+                        colors = ButtonDefaults.buttonColors (
+                            containerColor = MaterialTheme.colorScheme.inverseSurface,
+                        ),
+                        onClick = { showDialog = false }
+                    ) {
+                        Text(text = "ok")
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun EmptyCartMessage(navCont: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Ups, tu carrito está vacío",
+            style = MaterialTheme.typography.labelSmall,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        ExtendedFloatingActionButton(
+            modifier = Modifier
+                .padding(14.dp),
+            onClick = { navCont.navigate(route="readmenu")},
+            icon = { Icon(imageVector = ImageVector.vectorResource(id = R.drawable.baseline_menu_book_24),
+                    contentDescription ="carta",
+                    modifier=Modifier.size(30.dp)
+                    )
+                   },
+            text = { Text(text = "Volver al menu de platos") },
+        )
     }
 }
